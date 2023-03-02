bf0c96caa8b3296deafe360f6c242e2a170fcb8b
Nathaniel Iverson <niverson@sienaheights.edu>
2023-03-02 10:33:44 -0500

    Add project files.

---
+++ DovesCry/DovesCry.py
@@ -0,0 +1,2 @@
+from lyric import *
+doves()
---
+++ DovesCry/lyric.py
@@ -0,0 +1,8 @@
+def doves():
+    print("This is what is sounds like... when doves cry")
+
+def main():
+    print("main program is lyric")
+
+if __name__ == "__main__":
+    main()
